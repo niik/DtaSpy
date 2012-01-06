0ac3e03ac16e057e864558f07332b3487c2cbc44
Markus Olsson <j.markus.olsson@gmail.com>
2012-01-06 22:02:27 +0100

    Performance improvements in BizTalkTrackedMessagePartFragment
     * If ImagePart is available we don't have to load the first
       fragment
    
     * Renamed SaveTo to WriteTo
    
     * Refactored get accessor of Fragments-property into own method
    
     * Made new LoadFragments method use yield
    
     * Allow streaming scenarios with near-constant memory usage
       of large (massive) messages using WriteTo and cacheFragments
       parameter

--- src/DtaSpy/Message/BizTalkTrackedMessagePart.cs
+++ src/DtaSpy/Message/BizTalkTrackedMessagePart.cs
@@ -35,39 +35,93 @@ namespace DtaSpy
             {
                 if (_fragments == null)
                 {
-                    var fragments = new List<BizTalkTrackedMessagePartFragment>();
+                    _fragments = new List<BizTalkTrackedMessagePartFragment>();
 
-                    for (int i = 1; i <= FragmentCount; i++)
-                    {
-                        var fragment = this.db.LoadTrackedPartFragment(this.PartId, i, this.spoolId);
-
-                        if (fragment == null)
-                            break;
-
-                        fragments.Add(fragment);
-                    }
-
-                    _fragments = fragments;
+                    foreach (var fragment in LoadFragments())
+                        _fragments.Add(fragment);
                 }
 
                 return _fragments;
             }
         }
 
-        public int SaveTo(Stream s)
+        private IEnumerable<BizTalkTrackedMessagePartFragment> LoadFragments()
+        {
+            var fragments = new List<BizTalkTrackedMessagePartFragment>();
+
+            int startFragment = 1;
+
+            if (ImagePart != null)
+            {
+                // We already have the first fragment
+                yield return new BizTalkTrackedMessagePartFragment(this.db, this.spoolId)
+                {
+                    ImagePart = this.ImagePart
+                };
+
+                // Don't load fragment #1 from db, we already have it.
+                startFragment = 2;
+            }
+
+            for (int i = startFragment; i <= FragmentCount; i++)
+            {
+                var fragment = this.db.LoadTrackedPartFragment(this.PartId, i, this.spoolId);
+
+                if (fragment == null)
+                {
+                    var io = new InvalidCastException("Could not find fragment " + i + " of " + FragmentCount + " for message part");
+                    io.Data["partId"] = this.PartId;
+
+                    throw io;
+                }
+
+                yield return fragment;
+            }
+
+            _fragments = fragments;
+        }
+
+        /// <summary>
+        /// Writes the decoded contents of all fragments to the given stream while caching
+        /// loaded fragments for future access. Not caching improved memory usage and may improve
+        /// performance so if you're never going to access the fragments after this call, please use
+        /// the overload which allows you to specify whether to cache or not.
+        /// </summary>
+        public int WriteTo(Stream s)
+        {
+            return WriteTo(s, true);
+        }
+
+        /// <summary>
+        /// Writes the decoded contents of all fragments to the given stream, optionally caching
+        /// loaded fragments for future access. Not caching improved memory usage and may improve
+        /// performance. Don't cache if you're never going to access the fragments after. Caching
+        /// has no effect if fragments already have been loaded for this message part.
+        /// </summary>
+        /// <param name="s">The s.</param>
+        /// <param name="cacheFragments">
+        /// if set to <c>true</c> cache fragments for future access 
+        /// via the Fragments property.
+        /// </param>
+        public int WriteTo(Stream s, bool cacheFragments)
         {
             int total = 0;
 
-            // TODO: If fragmentcount == 1 then we can probably skip the iteration and go 
-            // straight to our own ImagePart.
-            foreach (var fragment in this.Fragments)
+            IEnumerable<BizTalkTrackedMessagePartFragment> fragments;
+
+            if (!cacheFragments || this._fragments != null)
+                fragments = this.Fragments;
+            else
+                fragments = LoadFragments();
+
+            byte[] buf = new byte[8192];
+            int c;
+
+            foreach (var fragment in fragments)
             {
                 using (var ds = new MemoryStream(fragment.ImagePart))
                 using (var bz = new BizTalkFragmentStream(ds, CompressionMode.Decompress))
                 {
-                    byte[] buf = new byte[8192];
-                    int c;
-
                     while ((c = bz.Read(buf, 0, buf.Length)) > 0)
                     {
                         s.Write(buf, 0, c);
