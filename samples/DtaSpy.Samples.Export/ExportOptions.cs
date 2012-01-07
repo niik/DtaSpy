a14ceb07851777c25138b18d7967bde53ad2f593
Markus Olsson <j.markus.olsson@gmail.com>
2012-01-07 01:19:39 +0100

    Add sample export project
    Sample console project that exports messages (all or single based
    on message id) to file. This is not meant to be a bullet-proof
    solution, it's simply meant to demonstrate the library.

---
+++ samples/DtaSpy.Samples.Export/ExportOptions.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Configuration;
+using System.Data.SqlClient;
+using NDesk.Options;
+
+namespace DtaSpy.Samples.Export
+{
+    public class ExportOptions
+    {
+        public static string DefaultConnectionString
+        {
+            get
+            {
+                var connectionString = ConfigurationManager.ConnectionStrings["default"];
+
+                if (connectionString == null)
+                    return null;
+
+                return connectionString.ConnectionString;
+            }
+        }
+
+        private OptionSet optionSet;
+        private SqlConnectionStringBuilder connectionStringBuilder;
+
+        public bool Help { get; set; }
+        public bool ExportAll { get; set; }
+        public bool ExportContext { get; set; }
+        public Guid MessageId { get; set; }
+        public bool PrintDebugInfo { get; set; }
+        public string ConnectionString { get; set; }
+
+        public ExportOptions()
+        {
+            this.connectionStringBuilder = new SqlConnectionStringBuilder(DefaultConnectionString ?? string.Empty);
+
+            // If there's no connection string in the config file we'll assume integrated
+            // security. Mostly because we have no way of specifiying username and password
+            // in the command options.
+            if (string.IsNullOrEmpty(DefaultConnectionString))
+                this.connectionStringBuilder.IntegratedSecurity = true;
+
+            // Option names partly stolen from http://devlicio.us/blogs/rob_reynolds/archive/2009/11/22/command-line-parsing-with-mono-options.aspx
+            this.optionSet = new OptionSet();
+
+            optionSet.Add("?|help|h", "Prints out the options.", option => this.Help = option != null);
+            optionSet.Add("debug", "Print debug info.", option => this.PrintDebugInfo = option != null);
+            optionSet.Add("d=|db=|database=|databasename=", "The name of the DTA database you want to connect to.", option => this.connectionStringBuilder.InitialCatalog = option);
+            optionSet.Add("s=|server=|servername=|instance=|instancename=", string.Format("The server and instance you would like to run on. (local) and (local)\\SQL2008 are both valid values. Defaults to \"{0}\".", this.connectionStringBuilder.DataSource), option => this.connectionStringBuilder.DataSource = option);
+            optionSet.Add("c=|connectionstring=", "The connection string to use when connecting to the DTA database", option => this.connectionStringBuilder= new SqlConnectionStringBuilder(option));
+            optionSet.Add("id=|message-id=", "The id of the message to export", option => this.MessageId = new Guid(option));
+            optionSet.Add("context", "Export the message context as well (experimental)", option => this.ExportContext = option != null);
+            optionSet.Add("all", "Export all messages in the tracking database", option => this.ExportAll = option != null);
+        }
+
+        public bool Parse(string[] args)
+        {
+            try
+            {
+                optionSet.Parse(args);
+                this.ConnectionString = this.connectionStringBuilder.ConnectionString;
+            }
+            catch (OptionException)
+            {
+                PrintCommandLineHelp("Error - usage is:");
+                return false;
+            }
+
+            return true;
+        }
+
+        public int PrintCommandLineHelp(string message)
+        {
+            Console.Error.WriteLine(message);
+            this.optionSet.WriteOptionDescriptions(Console.Error);
+
+            return -1;
+        }
+    }
+}
---
+++ samples/DtaSpy.Samples.Export/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Xml;
+
+namespace DtaSpy.Samples.Export
+{
+    public static class Program
+    {
+        private static BizTalkTrackingDb db;
+
+        private static ExportOptions options;
+
+        public static int Main(string[] args)
+        {
+            options = new ExportOptions();
+
+            if (!options.Parse(args))
+                return -1;
+            
+            if (options.Help || args.Length == 0)
+            {
+                Console.Error.WriteLine("DtaSpy Export Sample");
+                return options.PrintCommandLineHelp("DtaSpy.Samples.Export.exe [options]");
+            }
+
+            Debug("Using connection string " + options.ConnectionString);
+
+            db = new BizTalkTrackingDb(options.ConnectionString);
+
+            if (options.ExportAll)
+            {
+                foreach (var message in db.LoadTrackedMessages())
+                {
+                    using (message)
+                        Export(message, options.ExportContext);
+                }
+            }
+            else
+            {
+                if (options.MessageId == Guid.Empty)
+                    return options.PrintCommandLineHelp("Must specify either message id (--message-id) or --all");
+
+                var message = db.LoadTrackedMessage(options.MessageId);
+
+                if (message == null)
+                {
+                    Console.Error.WriteLine("No message found for id " + options.MessageId);
+                    return -1;
+                }
+
+                Export(message, options.ExportContext);
+            }
+
+            return 0;
+        }
+
+        private static void Debug(string message)
+        {
+            if (!options.PrintDebugInfo)
+                return;
+
+            Console.Error.WriteLine(message);
+        }
+
+        private static void Export(BizTalkTrackedMessage message, bool exportContext)
+        {
+            ExportMessage(message);
+
+            if (exportContext)
+                ExportContext(message);
+        }
+
+        private static void ExportMessage(BizTalkTrackedMessage message)
+        {
+            int partNo = 1;
+            foreach (var part in message.Parts)
+            {
+                Console.Error.WriteLine("Exporting " + message.MessageId + " part " + partNo + " of " + message.PartCount);
+
+                using (var fs = File.OpenWrite(GetMessagePartExportFilename(message, part)))
+                    part.WriteTo(fs, false);
+
+                partNo++;
+            }
+        }
+
+        /// <summary>
+        /// Attempts to produce the same context xml format as the built-in tooling in BizTalk. Note
+        /// that it is not complete and highly experimental.
+        /// </summary>
+        private static void ExportContext(BizTalkTrackedMessage message)
+        {
+            Console.Error.WriteLine("Exporting " + message.MessageId + " context");
+
+            using (var context = db.LoadTrackedMessageContext(message.MessageId, message.SpoolId))
+            using (var writer = new XmlTextWriter(GetMessageContextExportFilename(message), Encoding.UTF8))
+            {
+                writer.Formatting = Formatting.Indented;
+
+                writer.WriteStartElement("MessageInfo");
+
+                writer.WriteStartElement("ContextInfo");
+                writer.WriteAttributeString("PropertiesCount", context.Properties.Count.ToString());
+
+                foreach (var property in context.Properties)
+                {
+                    var arr = property.Value as Array;
+                    if (arr != null)
+                    {
+                        writer.WriteStartElement("ArrayProperty");
+                        writer.WriteAttributeString("Name", property.Name);
+                        writer.WriteAttributeString("Namespace", property.Namespace);
+
+                        for (int i = 0; i < arr.Length; i++)
+                        {
+                            writer.WriteStartElement("ArrayElement" + (i + 1));
+                            writer.WriteAttributeString("Value", arr.GetValue(i).ToString());
+
+                            writer.WriteEndElement();
+                        }
+
+                        writer.WriteEndElement();
+                    }
+                    else
+                    {
+                        writer.WriteStartElement("Property");
+                        
+                        writer.WriteAttributeString("Name", property.Name);
+                        writer.WriteAttributeString("Namespace", property.Namespace);
+                        writer.WriteAttributeString("Value", property.Value.ToString());
+
+                        writer.WriteEndElement();
+                    }
+                }
+
+                writer.WriteEndElement();
+
+                writer.WriteStartElement("PartInfo");
+                writer.WriteAttributeString("PartsCount", message.PartCount.ToString());
+
+                foreach (var part in message.Parts)
+                {
+                    writer.WriteStartElement("MessagePart");
+
+                    writer.WriteAttributeString("ID", "{" + part.PartId + "}");
+                    writer.WriteAttributeString("Name", part.PartName);
+                    writer.WriteAttributeString("FileName", Path.GetFullPath(GetMessagePartExportFilename(message, part)));
+
+                    writer.WriteAttributeString("Charset", "");
+                    writer.WriteAttributeString("ContentType", "");
+
+                    writer.WriteEndElement();
+                }
+
+                writer.WriteEndElement();
+
+                writer.WriteEndElement();
+            }
+        }
+
+        private static string GetMessagePartExportFilename(BizTalkTrackedMessage message, BizTalkTrackedMessagePart part)
+        {
+            return "{" + message.MessageId + "}_{" + part.PartId + "}" + (part.PartName == "" ? "" : "_" + part.PartName) + ".out";
+        }
+
+        private static string GetMessageContextExportFilename(BizTalkTrackedMessage message)
+        {
+            return "{" + message.MessageId + "}_context.xml";
+        }
+    }
+}
